c5aefd18fe420f5394d9b330f74f69f15223cb08
Parsla Bogdanova <meParsla@example.com>
2024-10-02 21:36:09 +0300

    Did all the task of this .php file

---
+++ blog.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tiešsaistes dienasgrāmata</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="index.php">Piesakies webināram!</a>
+        </nav>
+    </header>
+    <div>
+        <h1>Biogrāfija</h1>
+    </div>
+    <div>
+        <h2 class="small-header">
+            Kāpēc es nolēmu kļūt par programmētaju?
+        </h2>
+        <ul>
+            <li>Vecāki man sniedza ietiakumu</li>
+            <li>Vēlējos izmēģināt jaunas lietas, ko agrāk nebiju mēģinājusi</li>
+            <li>Prakses/pirms prakses laikā uzzināju, ka kiberdrošība ir, tāpēc esmu ienteresēts par to uzzināt/iemācīties.</li>
+            <li>Man patīk back-end un front-end lietass, ja vien man is sava personālā telps. Vieglāk mācīties.</li>
+            <li>Prolēmu risināšāna ir tas, kas man patīk darīt.</li>
+        </ul>
+    </div>
+    
+</body>
+</html>
